5ff386fbc1461cc7d2a07cb9ccb578740ab5889f
Harry <harry.n.griffiths@gmail.com>
2025-05-25 20:17:49 +0100

    For real this time fixed PNLMS and IPNLMS

--- src/Estimators/AdaptiveFilters.py
+++ src/Estimators/AdaptiveFilters.py
@@ -45,6 +45,9 @@ class LMS(AdaptiveFilter):
         y_hat = np.dot(self.w, self._delay_line)
         error = y_sample - y_hat
         self.w = np.add(self.w, (self.mu * error * self._delay_line))
+        if abs(error) > 1e2 or np.any(np.abs(self.w) > 1e3):
+            print(f"Huge update at sample")
+            print(f"x: {x_sample}, error: {error}, w: {self.w}")
         return y_hat, error
     
     def reset(self):
@@ -78,12 +81,10 @@ class PNLMS(NLMS):
         self._delay_line[0] = x_sample
         y_hat = np.dot(np.conjugate(self.w), self._delay_line)
         error = y_sample - y_hat
-        l = np.amax(np.abs(self.w))
-        l = max(l, self._delta)
-        g = (lambda w_i : (np.maximum(self._p * l, np.abs(w_i))))(self.w)
-        g_mean = np.mean(g)
-        sigma = np.mean(np.square(self._delay_line)) + 1e-12
-        self.w = np.add(self.w, (self.mu / self.tap_count)*(g/g_mean)*((error*self._delay_line)/sigma))
+        w_max = np.amax(np.abs(self.w))
+        gamma = (lambda w_i: (np.maximum(self._p * max(self._delta, w_max), np.abs(w_i)))) (self.w)
+        g = gamma / np.sum(gamma)
+        self.w = np.add(self.w, (self.mu * g * self._delay_line * error) / (np.sum(g * self._delay_line**2) + self._delta))
         return y_hat, error
 
 class IPNLMS(PNLMS):
@@ -99,5 +100,5 @@ class IPNLMS(PNLMS):
         error = y_sample - y_hat
         w_norm = np.sum(np.abs(self.w))
         k = (lambda w_i : ((1-self.alpha) / (2*self.tap_count)) + (1+self.alpha)*((np.abs(w_i))/(2*w_norm + 1e-12))) (self.w)
-        self.w = np.add(self.w, (self.mu * k * self._delay_line * error) / (self._delay_line * k * self._delay_line + self._delta))
+        self.w = np.add(self.w, (self.mu * k * self._delay_line * error) / (np.sum(k * self._delay_line**2) + self._delta))
         return y_hat, error
